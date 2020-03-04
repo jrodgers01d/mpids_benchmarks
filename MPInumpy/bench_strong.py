80c8f28f4505385a988557a911e3f76da4bd20b3
felix01d <johnrodgers01d@gmail.com>
2020-03-03 23:21:55 -0600

    MPInumpy scaling

---
+++ MPInumpy/bench_strong.py
@@ -0,0 +1,141 @@
+from mpi4py import MPI
+import numpy as np
+import mpids.MPInumpy as mpi_np
+import gc
+
+measure_time = lambda: MPI.Wtime()
+
+def creation(size, iters=10000, comm=MPI.COMM_WORLD):
+    data = np.arange(size, dtype=np.float64).tolist()
+    time = measure_time()
+    for _ in range(iters):
+        mpi_np.array(data, dtype=np.float64)
+    time = measure_time() - time
+    gc.collect()
+    comm.reduce(time, op=MPI.MAX, root=0)
+    return time/iters
+
+def add(mpi_np_arr, iters=10000, comm=MPI.COMM_WORLD):
+    time = measure_time()
+    for _ in range(iters):
+        mpi_np_arr + 1.0
+    time = measure_time() - time
+    comm.reduce(time, op=MPI.MAX, root=0)
+    return time/iters
+
+def sub(mpi_np_arr, iters=10000, comm=MPI.COMM_WORLD):
+    time = measure_time()
+    for _ in range(iters):
+        mpi_np_arr - 1.0
+    time = measure_time() - time
+    comm.reduce(time, op=MPI.MAX, root=0)
+    return time/iters
+
+def mul(mpi_np_arr, iters=10000, comm=MPI.COMM_WORLD):
+    time = measure_time()
+    for _ in range(iters):
+        mpi_np_arr * 1.0
+    time = measure_time() - time
+    comm.reduce(time, op=MPI.MAX, root=0)
+    return time/iters
+
+def div(mpi_np_arr, iters=10000):
+    time = measure_time()
+    for _ in range(iters):
+        mpi_np_arr / 1.0
+    time = measure_time() - time
+    comm.reduce(time, op=MPI.MAX, root=0)
+    return time/iters
+
+def _max(mpi_np_arr, iters=10000, axis=None, comm=MPI.COMM_WORLD):
+    time = measure_time()
+    for _ in range(iters):
+        mpi_np_arr.max(axis=axis)
+    time = measure_time() - time
+    comm.reduce(time, op=MPI.MAX, root=0)
+    return time/iters
+
+
+def _mean(mpi_np_arr, iters=10000, axis=None, comm=MPI.COMM_WORLD):
+    time = measure_time()
+    for _ in range(iters):
+        mpi_np_arr.mean(axis=axis)
+    time = measure_time() - time
+    comm.reduce(time, op=MPI.MAX, root=0)
+    return time/iters
+
+def _sum(mpi_np_arr, iters=10000, axis=None, comm=MPI.COMM_WORLD):
+    time = measure_time()
+    for _ in range(iters):
+        mpi_np_arr.sum(axis=axis)
+    time = measure_time() - time
+    comm.reduce(time, op=MPI.MAX, root=0)
+    return time/iters
+
+def _std(mpi_np_arr, iters=10000, axis=None, comm=MPI.COMM_WORLD):
+    time = measure_time()
+    for _ in range(iters):
+        mpi_np_arr.std(axis=axis)
+    time = measure_time() - time
+    comm.reduce(time, op=MPI.MAX, root=0)
+    return time/iters
+
+def slicing(mpi_np_arr, iters=10000, comm=MPI.COMM_WORLD):
+    time = measure_time()
+    for _ in range(iters):
+        mpi_np_arr[::2]
+    time = measure_time() - time
+    comm.reduce(time, op=MPI.MAX, root=0)
+    return time/iters
+
+def iterate(mpi_np_arr, iters=10000, comm=MPI.COMM_WORLD):
+    time = measure_time()
+    for _ in range(iters):
+        for val in mpi_np_arr:
+            pass
+    time = measure_time() - time
+    comm.reduce(time, op=MPI.MAX, root=0)
+    return time/iters
+
+def reshape(mpi_np_arr, size, iters=10000, comm=MPI.COMM_WORLD):
+    time = measure_time()
+    for _ in range(iters):
+        mpi_np_arr.reshape(size, 1)
+    time = measure_time() - time
+    comm.reduce(time, op=MPI.MAX, root=0)
+    return time/iters
+
+if __name__ == '__main__':
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+    n_procs = comm.Get_size()
+    size = 2**26
+    iters = 1
+    mpi_np_arr = mpi_np.arange(size, dtype=np.float64)
+
+    creation_time = creation(size, iters=iters)
+    add_time = add(mpi_np_arr, iters=iters)
+    sub_time = sub(mpi_np_arr, iters=iters)
+    mul_time = mul(mpi_np_arr, iters=iters)
+    div_time = div(mpi_np_arr, iters=iters)
+    max_time = _max(mpi_np_arr, iters=iters)
+    mean_time = _mean(mpi_np_arr, iters=iters)
+    sum_time = _sum(mpi_np_arr, iters=iters)
+    std_time = _std(mpi_np_arr, iters=iters)
+    slicing_time = slicing(mpi_np_arr, iters=iters)
+    iterate_time = iterate(mpi_np_arr, iters=iters)
+    reshape_time = reshape(mpi_np_arr, size, iters=iters)
+
+    if rank == 0:
+        print("mpi_np,creation,%d,%d,%.9f" %(n_procs, size, creation_time))
+        print("mpi_np,add,%d,%d,%.9f" %(n_procs, size, add_time))
+        print("mpi_np,sub,%d,%d,%.9f" %(n_procs, size, sub_time))
+        print("mpi_np,mul,%d,%d,%.9f" %(n_procs, size, mul_time))
+        print("mpi_np,div,%d,%d,%.9f" %(n_procs, size, div_time))
+        print("mpi_np,max,%d,%d,%.9f" %(n_procs, size, max_time))
+        print("mpi_np,mean,%d,%d,%.9f" %(n_procs, size, mean_time))
+        print("mpi_np,sum,%d,%d,%.9f" %(n_procs, size, sum_time))
+        print("mpi_np,std,%d,%d,%.9f" %(n_procs, size, std_time))
+        print("mpi_np,slice,%d,%d,%.9f" %(n_procs, size, slicing_time))
+        print("mpi_np,iterate,%d,%d,%.9f" %(n_procs, size, iterate_time))
+        print("mpi_np,reshape,%d,%d,%.9f" %(n_procs, size, reshape_time))
---
+++ MPInumpy/run_trials.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH -N 11
+#SBATCH --job-name=np_v_mpi_np
+#SBATCH --ntasks-per-node 48
+#SBATCH -o np_v_mpi_np.%j.stdout
+#SBATCH -e np_v_mpi_np.%j.error
+#SBATCH -x crill-001
+#SBATCH --exclusive
+#SBATCH -t 10:00:00
+#SBATCH -p crill
+
+module load mpi4py/3.0.0-python3.4
+
+HEADER_SCALING="\"Type\",\"Op\",\"Num_Proc\",\"Size\",\"Time\"";
+DATE=$(date +%Y%m%d_%H_%M_%S);
+
+echo $HEADER_SCALING > mpi_np_strong_${DATE}.csv
+echo $HEADER_SCALING > mpi_np_weak_${DATE}.csv
+for run in {1..50};do
+  for cores in 1 2 4 8 16 32 64 128 256 512;do
+    mpiexec -n $cores python3 ./bench_strong.py >> mpi_np_strong_${DATE}.csv
+    mpiexec -n $cores python3 ./bench_weak.py >> mpi_np_weak_${DATE}.csv
+  done
+done
