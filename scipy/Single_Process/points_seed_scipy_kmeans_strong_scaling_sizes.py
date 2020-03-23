70f297d73d5e6a869a6a43128ded839f37559878
felix01d <johnrodgers01d@gmail.com>
2020-03-23 16:52:07 -0500

    Changing number of iterations to ensure convergence

--- scipy/Single_Process/points_seed_scipy_kmeans_strong_scaling_sizes.py
+++ scipy/Single_Process/points_seed_scipy_kmeans_strong_scaling_sizes.py
@@ -5,8 +5,7 @@ from operations import gen_blobs, measure_time
 if __name__ == '__main__':
     t_time = measure_time()
     print("Type,Obs,Feats,Clusters,Time")
-    for obs_power in range(15, 21):
-    # for obs_power in [25]:
+    for obs_power in [15, 16, 17, 18, 19, 20, 25]:
         num_obs = 2**obs_power
         features = 2
         k = 2
@@ -14,7 +13,7 @@ if __name__ == '__main__':
         observations, labels = gen_blobs(num_obs, features, k)
         for _ in range(5):
             time = measure_time()
-            centroids, labels = cluster.kmeans2(observations, k, iter=10, minit='points')
+            centroids, labels = cluster.kmeans2(observations, k, iter=100, minit='points')
             time = measure_time() - time
 
             print("scipy,%d,%d,%d,%.9f" %(num_obs, features, k, time))
