0dfaa72a0b880ae30d9ab52c5941411d1c7d6587
felix01d <johnrodgers01d@gmail.com>
2020-03-23 16:34:25 -0500

    Quick comparison benchmark

---
+++ scipy/Single_Process/points_seed_scipy_kmeans_strong_scaling_sizes.py
@@ -0,0 +1,23 @@
+import numpy as np
+import scipy.cluster.vq as cluster
+from operations import gen_blobs, measure_time
+
+if __name__ == '__main__':
+    t_time = measure_time()
+    print("Type,Obs,Feats,Clusters,Time")
+    for obs_power in range(15, 21):
+    # for obs_power in [25]:
+        num_obs = 2**obs_power
+        features = 2
+        k = 2
+
+        observations, labels = gen_blobs(num_obs, features, k)
+        for _ in range(5):
+            time = measure_time()
+            centroids, labels = cluster.kmeans2(observations, k, iter=10, minit='points')
+            time = measure_time() - time
+
+            print("scipy,%d,%d,%d,%.9f" %(num_obs, features, k, time))
+
+    total_time = measure_time() - t_time
+    print(total_time)
